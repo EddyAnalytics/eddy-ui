4df14deb1cc05ee96fecda9414dbbb1cac069030
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2019-10-04 19:54:08 +0200

    Add tabbed flink sql block properties

---
+++ src/components/pipeline/block-properties/FlinkSQLProperties.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <b-tabs position="is-centered is-boxed" :animated="false" :destroy-on-hide="true">
+            <b-tab-item label="Input Schema">
+                <div class="codemirror-wrapper">
+                    <codemirror v-model="properties.inSchema" :options="inSchemaOptions" />
+                </div>
+            </b-tab-item>
+            <b-tab-item label="Output schema">
+                <div class="codemirror-wrapper">
+                    <codemirror v-model="properties.outSchema" :options="outSchemaOptions" />
+                </div>
+            </b-tab-item>
+            <b-tab-item label="FinkSQL Query">
+                <div class="codemirror-wrapper">
+                    <codemirror v-model="properties.sqlQuery" :options="sqlQueryOptions" />
+                </div>
+            </b-tab-item>
+        </b-tabs>
+    </div>
+</template>
+
+<script>
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+import { codemirror } from 'vue-codemirror';
+import 'codemirror/mode/sql/sql.js';
+import 'codemirror/mode/javascript/javascript.js';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/neo.css';
+
+@Component({
+    components: {
+        codemirror,
+    },
+})
+export default class PipelineBlockForm extends Vue {
+    @Prop() properties;
+
+    options = {
+        theme: 'neo',
+        lineNumbers: true,
+    };
+
+    inSchemaOptions = {
+        ...this.options,
+        mode: 'application/ld+json',
+    };
+
+    outSchemaOptions = {
+        ...this.options,
+        mode: 'application/ld+json',
+    };
+
+    sqlQueryOptions = {
+        ...this.options,
+        mode: 'text/x-sql',
+    };
+}
+</script>
