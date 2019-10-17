02a8ca7fc543f088cab5a7f78e121faff6e57a8b
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2019-10-17 13:28:19 +0200

    Add basic Flink clusters info

---
+++ src/components/settings/FlinkClusterInfoBlock.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- <div>{{ info }}</div> -->
+        <div>
+            <strong>Flink version:</strong>
+            {{ info[0]['flink-version'] }}
+        </div>
+        <div>
+            <strong>Jobs:</strong>
+            <div v-for="job in info[1]['jobs']" :key="job.id">{{ job }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class FlinkClusterInfoBlock extends Vue {
+    @Prop() info;
+}
+</script>
--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,11 +1,42 @@
 <template>
-    <main class="container">
+    <main class="container-fluid">
         <section class="section">
-            <h1 class="title is-1 is-spaced">Settings</h1>
+            <div class="columns">
+                <div class="column">
+                    <h1 class="title is-1 is-spaced">Settings</h1>
+                </div>
+                <div class="column has-text-right">
+                    <b-button outlined type="is-info" icon-left="reload" @click="refresh()">
+                        Refresh
+                    </b-button>
+                </div>
+            </div>
+        </section>
+        <section class="section">
             <h2 class="subtitle is-3">System Status</h2>
-            <h2 class="subtitle is-3">System Settings</h2>
+        </section>
+        <section class="section">
+            <h2 class="subtitle is-3">Flink Cluster</h2>
+            <div v-if="flinkInfo">
+                <flink-cluster-info-block :info="flinkInfo" />
+            </div>
+        </section>
+        <section class="section">
+            <h2 class="subtitle is-3">Flink Legacy Cluster</h2>
+            <div v-if="flinkLegacyInfo">
+                <flink-cluster-info-block :info="flinkLegacyInfo" />
+            </div>
+        </section>
+        <section class="section">
+            <h2 class="subtitle is-3">Debezium</h2>
+            <div>
+                {{ debeziumInfo }}
+            </div>
+        </section>
+        <section class="section">
             <h2 class="subtitle is-3">Kafka</h2>
-            <h2 class="subtitle is-3">Flink</h2>
+        </section>
+        <section class="section">
             <h2 class="subtitle is-3">Spark</h2>
         </section>
     </main>
@@ -14,7 +45,50 @@
 <script>
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
+import INTEGRATIONS_QUERY from '@/graphql/queries/integrations.gql';
+import DATA_CONNECTORS_QUERY from '@/graphql/queries/dataConnectors.gql';
 
-@Component
-export default class Settings extends Vue {}
+import FlinkClusterInfoBlock from '@/components/settings/FlinkClusterInfoBlock';
+
+@Component({
+    components: {
+        FlinkClusterInfoBlock,
+    },
+})
+export default class Settings extends Vue {
+    debeziumURL = '';
+    debeziumInfo = null;
+    flinkURL = 'https://flink.k.eddy-analytics.org/v1/';
+    flinkInfo = null;
+    flinkLegacyURL = 'https://flink18.k.eddy-analytics.org/v1/';
+    flinkLegacyInfo = null;
+
+    created() {
+        this.$apollo.addSmartQuery('allIntegrations', {
+            query: INTEGRATIONS_QUERY,
+        });
+        this.$apollo.addSmartQuery('dataConnectors', {
+            query: DATA_CONNECTORS_QUERY,
+        });
+
+        this.refresh();
+    }
+
+    refresh() {
+        this.fetchFlinkInfo();
+    }
+
+    async fetchFlinkInfo() {
+        try {
+            const urls = ['config', 'jobs'];
+            const promises = urls.map(url => this.axios.get(this.flinkURL + url));
+            this.flinkInfo = await Promise.all(promises);
+            const legacyPromises = urls.map(url => this.axios.get(this.flinkLegacyURL + url));
+            this.flinkLegacyInfo = await Promise.all(legacyPromises);
+        } catch (e) {
+            console.log('Error while fetching Flink info', e);
+            throw e;
+        }
+    }
+}
 </script>
