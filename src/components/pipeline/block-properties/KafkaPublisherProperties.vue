bcfdbc4c19f059bd6e1cfbf0b25b52bbc644bf8d
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2019-10-18 21:59:50 +0200

    Add Kafka publisher and subscriber block properties  topic search

--- src/components/pipeline/block-properties/KafkaPublisherProperties.vue
+++ src/components/pipeline/block-properties/KafkaPublisherProperties.vue
@@ -1,17 +1,54 @@
 <template>
     <div>
         <h2 class="subtitle">Kafka Publisher</h2>
+
         <b-field label="Topic">
-            <b-input v-model="properties.topic" />
+            <b-select v-model="properties.topic" :expanded="true">
+                <option v-for="topic in topics" :key="topic">
+                    {{ topic }}
+                </option>
+            </b-select>
+        </b-field>
+
+        <b-field label="Schema">
+            <schema-tree :schema="properties.schema" />
         </b-field>
     </div>
 </template>
 
 <script>
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import SchemaTree from '@/components/pipeline/block-properties/schema-tree/SchemaTree';
 
-@Component
+import KAFKA_TOPICS from '@/graphql/subscriptions/kafkaTopics.gql';
+
+@Component({
+    components: {
+        SchemaTree,
+    },
+})
 export default class KafkaPublisherProperties extends Vue {
     @Prop() properties;
+
+    topics = [];
+    created() {
+        this.$apollo.addSmartSubscription('topics', {
+            query: KAFKA_TOPICS,
+            result({ data: { topics } }) {
+                this.topics = topics;
+            },
+        });
+
+        if (!this.properties.schema) {
+            this.prefillSchema();
+        }
+    }
+
+    prefillSchema() {
+        this.$set(this.properties, 'schema', {
+            value: 'ROOT',
+            children: [{}],
+        });
+    }
 }
 </script>
--- src/components/pipeline/block-properties/KafkaSubscriberProperties.vue
+++ src/components/pipeline/block-properties/KafkaSubscriberProperties.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
         <h2 class="subtitle">Kafka Subscriber</h2>
+        {{ topics }}
         <b-field label="Topic">
-            <b-input v-model="properties.topic" />
+            <b-autocomplete
+                v-model="topic"
+                :data="filteredTopics"
+                @select="option => (properties.topic = option)"
+            ></b-autocomplete>
         </b-field>
     </div>
 </template>
@@ -10,8 +15,38 @@
 <script>
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
+import KAFKA_TOPICS from '@/graphql/subscriptions/kafkaTopics.gql';
+
 @Component
 export default class KafkaSubscriberProperties extends Vue {
     @Prop() properties;
+
+    topic = '';
+    topics = [];
+
+    created() {
+        this.$apollo.addSmartSubscription('topics', {
+            query: KAFKA_TOPICS,
+        });
+
+        if (this.properties.topic) {
+            this.topic = this.properties.topic;
+        }
+
+        if (!this.properties.schema) {
+            this.prefillSchema();
+        }
+    }
+
+    get filteredTopics() {
+        return this.topics.filter(topic => {
+            return (
+                topic
+                    .toString()
+                    .toLowerCase()
+                    .indexOf(this.topic.toLowerCase()) >= 0
+            );
+        });
+    }
 }
 </script>
