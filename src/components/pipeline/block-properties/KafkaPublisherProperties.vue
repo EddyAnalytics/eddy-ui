b59abfd84b0a62196b507d74aa01144119963091
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2019-10-21 17:35:18 +0200

    Change kafka publisher and subscriber properties forms

--- src/components/pipeline/block-properties/KafkaPublisherProperties.vue
+++ src/components/pipeline/block-properties/KafkaPublisherProperties.vue
@@ -1,54 +1,40 @@
 <template>
     <div>
         <h2 class="subtitle">Kafka Publisher</h2>
-
-        <b-field label="Topic">
-            <b-select v-model="properties.topic" :expanded="true">
-                <option v-for="topic in topics" :key="topic">
-                    {{ topic }}
-                </option>
-            </b-select>
-        </b-field>
-
-        <b-field label="Schema">
-            <schema-tree :schema="properties.schema" />
+        <b-field label="Topic" expanded>
+            <b-field>
+                <p class="control">
+                    <span class="button is-static">{{ this.projectId }}.</span>
+                </p>
+                <b-input v-model="properties.topicSlug" expanded></b-input>
+            </b-field>
         </b-field>
     </div>
 </template>
 
 <script>
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import SchemaTree from '@/components/pipeline/block-properties/schema-tree/SchemaTree';
+import { Component, Prop, Watch, Vue } from 'vue-property-decorator';
 
 import KAFKA_TOPICS from '@/graphql/subscriptions/kafkaTopics.gql';
 
-@Component({
-    components: {
-        SchemaTree,
-    },
-})
+@Component
 export default class KafkaPublisherProperties extends Vue {
     @Prop() properties;
 
     topics = [];
     created() {
+        this.projectId = +this.$route.params.projectId;
         this.$apollo.addSmartSubscription('topics', {
             query: KAFKA_TOPICS,
             result({ data: { topics } }) {
                 this.topics = topics;
             },
         });
-
-        if (!this.properties.schema) {
-            this.prefillSchema();
-        }
     }
 
-    prefillSchema() {
-        this.$set(this.properties, 'schema', {
-            value: 'ROOT',
-            children: [{}],
-        });
+    @Watch('properties.topicSlug')
+    onTopicSlugChange() {
+        this.properties.topic = this.projectId + '.' + this.properties.topicSlug;
     }
 }
 </script>
--- src/components/pipeline/block-properties/KafkaSubscriberProperties.vue
+++ src/components/pipeline/block-properties/KafkaSubscriberProperties.vue
@@ -1,32 +1,63 @@
 <template>
     <div>
         <h2 class="subtitle">Kafka Subscriber</h2>
-        {{ topics }}
         <b-field label="Topic">
             <b-autocomplete
                 v-model="topic"
+                open-on-focus
                 :data="filteredTopics"
                 @select="option => (properties.topic = option)"
             ></b-autocomplete>
         </b-field>
+
+        <b-tabs
+            v-model="tab"
+            position="is-centered is-boxed"
+            :animated="false"
+            :destroy-on-hide="true"
+        >
+            <b-tab-item label="Schema">
+                <b-field>
+                    <schema-tree :schema="properties.schema" />
+                </b-field>
+            </b-tab-item>
+            <b-tab-item label="Sample events">
+                <sample-events :topic="properties.topic" @fillSchema="fillSchema" />
+            </b-tab-item>
+        </b-tabs>
     </div>
 </template>
 
 <script>
 import { Component, Prop, Vue } from 'vue-property-decorator';
-
+import SchemaTree from '@/components/pipeline/block-properties/schema-tree/SchemaTree';
+import SampleEvents from '@/components/pipeline/block-properties/SampleEvents';
 import KAFKA_TOPICS from '@/graphql/subscriptions/kafkaTopics.gql';
 
-@Component
+@Component({
+    components: {
+        SchemaTree,
+        SampleEvents,
+    },
+})
 export default class KafkaSubscriberProperties extends Vue {
     @Prop() properties;
 
     topic = '';
     topics = [];
+    tab = 0;
 
     created() {
+        this.projectId = +this.$route.params.projectId;
         this.$apollo.addSmartSubscription('topics', {
             query: KAFKA_TOPICS,
+            result({ data: { topics } }) {
+                // Filter out the topics for this
+                this.topics = topics.filter(topic => {
+                    const parts = topic.split('.');
+                    return parts[0] && parts[0] == this.projectId;
+                });
+            },
         });
 
         if (this.properties.topic) {
@@ -48,5 +79,19 @@ export default class KafkaSubscriberProperties extends Vue {
             );
         });
     }
+
+    fillSchema(schema) {
+        this.prefillSchema(schema);
+        this.tab = 0;
+    }
+
+    prefillSchema(schema = this.DEFAULT_SCHEMA) {
+        this.$set(this.properties, 'schema', schema);
+    }
+
+    DEFAULT_SCHEMA = {
+        value: 'ROOT',
+        children: [{}],
+    };
 }
 </script>
